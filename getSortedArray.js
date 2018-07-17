984764b5bc809faa8ef48531968b4cbb9cc33c3a
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-07-18 01:20:03 +0200

    ok

--- getSortedArray.js
+++ getSortedArray.js
@@ -1,5 +1,27 @@
-function getSortedArray() {
-    console.log('getSortedArray');
+const getMinArray = require('./getMinArray');
+
+function getSortedArray(array) {
+    let result = [];
+    let a = array;
+
+    while (a.length > 0) {
+        const min = getMinArray(a);
+        result.push(min);
+
+        a = removeElement(a, min);
+    }
+    
+    return result;
+}
+
+function removeElement(array, element) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (element !== array[i]) {
+            result.push(array[i]);
+        }
+    }
+    return result;
 }
 
 module.exports = getSortedArray;
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const getMinArray = require('./getMinArray');
 const getSortedArray = require('./getSortedArray');
 
+function getReversedSortedArray(array) {
+    let sortedArray = getSortedArray(array);
+    const result = [];
+    for (let i = 0; i < sortedArray.length; i++) {
+        result[i] = sortedArray[sortedArray.length - 1 - i];
+    }
+
+    return result;
+}
+
 const array = [6, 3, 18, 10, 48, 2];
 
 const minimum = getMinArray(array);
@@ -8,3 +18,68 @@ console.log('minimum', minimum);
 
 const sortedArray = getSortedArray(array);
 console.log('sortedArray', sortedArray);
+
+const reversedSortedArray = getReversedSortedArray(array);
+console.log('reversedSortedArray', reversedSortedArray);
+
+function newArray(array, cb) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        result[i] = cb(array[i]);
+    }
+    return result;
+}
+
+const array2 = newArray(array, n => n * 3);
+console.log('array2', array2);
+
+function flat(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] instanceof Array) {
+            result = result.concat(flat(array[i]));
+        } else {
+            result.push(array[i]);
+        }
+    }
+
+    return result;
+}
+
+const array3 = [[[1], 2],[1, 8], 3];
+const flatArray = flat(array3);
+console.log('flatArray', flatArray);
+
+function reverse(array) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        result[i] = array[array.length - 1 - i];
+    }
+
+    return result;
+}
+
+const array4 = [12, 6, 36];
+const reversedArray = reverse(array4);
+console.log('reversedArray', reversedArray);
+
+function sum(array) {
+    let result = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        result = array[i] + result; 
+    }
+
+    return result;
+}
+
+const array5 = [3, 6, 10];
+const total = sum(array5);
+console.log('total', total);
+
+const total2 = array5.reduce((acc, n) => acc + n, 0);
+console.log('total2', total2);
+
+const total3 = array5.reduce((acc, n) => acc * n, 1);
+console.log('total3', total3);
+
