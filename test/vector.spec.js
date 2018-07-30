3171c51d0a37e9dd661109737b44535f9cc4881f
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-07-30 16:24:56 +0200

    ok

--- test/vector.spec.js
+++ test/vector.spec.js
@@ -5,9 +5,37 @@ const {
 
 describe.only('Vector', () => {
 
-    it('should vector', () => {
+    it('should euclidean norm', () => {
         const a = [3, 4];
         assert.deepStrictEqual(Vector.euclideanNorm(a), 5);
     });
+
+    it('should be a norm', () => {
+        const a = [0, 0, 0, 0, 0];
+        assert.deepStrictEqual(Vector.euclideanNorm(a), 0);
+        const u = [1, 2];
+        const v = [3, 4];
+        assert.ok(Vector.euclideanNorm(Vector.plus(u, v)) < Vector.euclideanNorm(u) + Vector.euclideanNorm(v));
+        const n = 3;
+        assert.ok(n * Vector.euclideanNorm(v) === Vector.euclideanNorm(Vector.scalarProduct(n, v)));
+
+    });
+
+    it('should be a norm', () => {
+        const a = [1, 2];
+        const b = [3, 4];
+        assert.deepStrictEqual(Vector.plus(a, b), [4, 6]);
+    });
+
+    it('should compute the scalarProduct', () => {
+        const a = [3, 4];
+        assert.deepStrictEqual(Vector.scalarProduct(2, a), [6, 8]);
+    });
+
+    it('should compute the manhattan norm', () => {
+        const a = [3, 1, 6, -2];
+        assert.deepStrictEqual(Vector.manhattanNorm(a), 12);
+    });
+
 
 });
--- vector.js
+++ vector.js
@@ -4,6 +4,18 @@ class Vector {
         return a.reduce((acc, n) => acc + n**2, 0)**0.5;
     }
 
+    static plus(a, b) {
+        return a.map((c, i) => a[i] + b[i]);
+    }
+
+    static scalarProduct(n, a) {
+        return a.map(r => r * n);
+    }
+
+    static manhattanNorm(a) {
+        return a.reduce((acc, n) => acc + Math.abs(n));
+    }
+
 }
 
 module.exports = {
