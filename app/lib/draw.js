f5d92bffe733a1bf9fb8af89a8e08757cd8160ee
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-08-03 12:12:46 +0200

    ok

--- app/lib/draw.js
+++ app/lib/draw.js
@@ -16,6 +16,13 @@ export function draw(fn) {
 
 }
 
+function normalize(x) {
+    if (Math.abs(x) < 1e-15) {
+        x = 0;
+    }
+    return x;
+}
+
 function bezierCurve(x, fn, incr) {
     const coef = 1 / 3;
     const dx = 2 * incr;
@@ -23,12 +30,16 @@ function bezierCurve(x, fn, incr) {
     const dy1 = fn(x) - fn(x - 2 * incr);
     const dy2 = fn(x + incr) - fn(x - incr);
 
-    const cx1 = x - incr + coef * incr;
-    const cy1 = fn(x - incr) + (dy1 / dx) * coef * incr;
-    const cx2 = x - coef * incr;
-    const cy2 = fn(x) - (dy2 / dx) * coef * incr;
+    const cx1 = normalize(x - incr + coef * incr);
+    const cy1 = normalize(fn(x - incr) + (dy1 / dx) * coef * incr);
+    const cx2 = normalize(x - coef * incr);
+    const cy2 = normalize(fn(x) - (dy2 / dx) * coef * incr);
 
-    return `C${cx1},${cy1} ${cx2},${cy2} ${x},${fn(x)} `;
+    x = normalize(x - coef * incr);
+    const y = normalize(fn(x));
+
+
+    return `C${cx1},${cy1} ${cx2},${cy2} ${x},${y} `;
 }
 
 
@@ -44,6 +55,7 @@ export function drawPath(svg, fn, start, end, incr, color) {
     for (let x = start + incr; x < end; x += incr) {
         d += bezierCurve(x, fn, incr);
     }
+    console.log('d', d);
     const path = document.createElementNS(ns, 'path');
     path.setAttribute('d', d);
     path.setAttribute('stroke-width', '0.4%');
