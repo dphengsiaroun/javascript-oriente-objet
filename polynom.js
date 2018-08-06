f4ffb02a45bc21c62d227b886883eb67edc459e0
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-08-06 17:12:10 +0200

    ok

--- polynom.js
+++ polynom.js
@@ -1,14 +1,8 @@
 class Polynom {
     static product(a, b) {
-        return new Array(a.length + b.length - 1).fill(0).map((n, i) => {
-            return new Array(i + 1).fill(0).reduce((acc, n, j) => {
-                const aj = a[j] ? a[j] : 0;
-                const bk = b[i - j] ? b[i - j] : 0;
-                console.log('i', i, 'j', j);
-                console.log(`${acc} + ${aj} * ${bk}`);
-                return acc + aj * bk;
-            }, 0);
-        })
+        return new Array(a.length + b.length - 1).fill(0).map((n, i) =>
+            new Array(i + 1).fill(0).reduce((acc, n, j) =>
+                acc + (a[j] ? a[j] : 0) * (b[i - j] ? b[i - j] : 0), 0))
     }
 }
 
--- test/polynom.spec.js
+++ test/polynom.spec.js
@@ -9,10 +9,16 @@ describe.only('Polynom', () => {
         const b = [-2, 1];
         assert.deepStrictEqual(Polynom.product(a, b), [-2, -1, 1]);
     });
-    
+
     it('should product', () => {
         const a = [1, 3, 5];
         const b = [4, 2, 1];
         assert.deepStrictEqual(Polynom.product(a, b), [4, 14, 27, 13, 5]);
+    });
+
+    it('should product', () => {
+        const a = [1, 0, 0, 0, 0, 1];
+        const b = [-1, 0, 0, 1];
+        assert.deepStrictEqual(Polynom.product(a, b), [-1, 0, 0, 1, 0, -1, 0, 0, 1]);
     });
 });
