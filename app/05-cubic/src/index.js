631715d3dc347cab8142c615c7bca2df517d53ca
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-08-08 11:50:55 +0200

    refactor

--- app/05-cubic/src/index.js
+++ app/05-cubic/src/index.js
@@ -5,6 +5,10 @@ import {
     drawPath
 } from '../../lib/draw';
 
+import {
+    Polynomial
+} from '../../../math/polynomial';
+
 const xstart = -10;
 const xend = 10;
 
@@ -27,51 +31,18 @@ document.querySelector('#grid').addEventListener('click', () => {
     } 
 });
 
-
-const format = (a, n, array) => {
-    const maxDegree = array.reduce((acc, n, i) => n !== 0 ? i : acc, 0);
-    if (a === 0) {
-        if (maxDegree === 0 && n === 0) {
-            return '0';
-        }
-        return '';
-    }
-    let result = '';
-    if (a >= 0 && n !== maxDegree) {
-        result += '+ ';
-    }
-    if (a < 0) {
-        result += '-';
-        if (n !== maxDegree) {
-            result += ' ';
-        }
-    }
-    
-    if (!(Math.abs(a) === 1 && n > 0)) {
-        result += `${Math.abs(a)}`;
-    }
-    if (n > 0) {
-        result += `x`;
-    }
-    if (n > 1) {
-        result += `<sup>${n}</sup>`;
-    }
-    return result;
-};
-const formatEquation = array => array.map((a, i, array) => format(a, i, array)).reverse().join(' ').trim();
-
 const a = [0, -5, 0, 0.5];
 const cubic = x => (a[3] * x ** 3) + (a[2] * x ** 2) + (a[1] * x) + a[0];
 let g = drawPath(graph, cubic, xstart, xend, step, 'green');
 const equationElt = document.querySelector('.equation');
-equationElt.innerHTML = formatEquation(a);
+equationElt.innerHTML = Polynomial.toString(a, 'html');
 
 for (let i = 0; i < 4; i++) {
     const elt = document.querySelector(`#a${i}`);
     elt.value = a[i];
     elt.addEventListener('input', (e) => {
         a[i] = +e.target.value;
-        equationElt.innerHTML = formatEquation(a);
+        equationElt.innerHTML = Polynomial.toString(a, 'html');
         const cubic = x => (a[3] * x ** 3) + (a[2] * x ** 2) + (a[1] * x) + a[0];
         g.remove();
         g = drawPath(graph, cubic, xstart, xend + incr, step, 'green');
